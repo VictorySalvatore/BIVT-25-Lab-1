28763061b70a7b6751896b1205b2c1ece49a882a
Потапова Виктория <125740061+VictorySalvatore@users.noreply.github.com>
2025-09-08 10:30:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Green white = new Green();
+            //Console.WriteLine(white.Task1(1.2));
+            //Console.WriteLine(blue.Task2(-3));
+            //Console.WriteLine(blue.Task3(25, 189));
+            //Console.WriteLine(white.Task4(-2.5, 1.89));
+            //Console.WriteLine(white.Task5(0.5));
+            //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            //Console.WriteLine(blue.Task7(25.1, -7));
+        }
+    }
+}
